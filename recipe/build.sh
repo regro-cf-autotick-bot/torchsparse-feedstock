5778c9ec250a311159d811d897aa1718739a04b7
Daniel Ching <dching@nvidia.com>
2026-09-02 12:40:37 -0500

    BLD: Use same CUDA arch list as pytorch package

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,23 +8,14 @@ if [[ "$cuda_compiler_version" == "None" ]]; then
   export FORCE_CUDA=0
 else
   export CUDA_NVCC_FLAGS="$CUDA_NVCC_FLAGS -O3"
-  export TORCH_CUDA_ARCH_LIST="3.5;5.0+PTX"
-  if [[ ${cuda_compiler_version} == 9.0* ]]; then
-      export TORCH_CUDA_ARCH_LIST="$TORCH_CUDA_ARCH_LIST;6.0;7.0"
-  elif [[ ${cuda_compiler_version} == 9.2* ]]; then
-      export TORCH_CUDA_ARCH_LIST="$TORCH_CUDA_ARCH_LIST;6.0;6.1;7.0"
-  elif [[ ${cuda_compiler_version} == 10.* ]]; then
-      export TORCH_CUDA_ARCH_LIST="$TORCH_CUDA_ARCH_LIST;6.0;6.1;7.0;7.5"
-  elif [[ ${cuda_compiler_version} == 11.0* ]]; then
-      export TORCH_CUDA_ARCH_LIST="$TORCH_CUDA_ARCH_LIST;6.0;6.1;7.0;7.5;8.0"
-  elif [[ ${cuda_compiler_version} == 11.1* ]]; then
-      export TORCH_CUDA_ARCH_LIST="$TORCH_CUDA_ARCH_LIST;6.0;6.1;7.0;7.5;8.0;8.6"
-  elif [[ ${cuda_compiler_version} == 11.2* ]]; then
-      export TORCH_CUDA_ARCH_LIST="$TORCH_CUDA_ARCH_LIST;6.0;6.1;7.0;7.5;8.0;8.6"
-  else
-      echo "unsupported cuda version. edit build.sh"
-      exit 1
+  # CF_TORCH_CUDA_ARCH_LIST is set by pytorch's activation scripts to match the arch list pytorch was built with.
+  # See https://github.com/conda-forge/pytorch-cpu-feedstock/blob/main/recipe/activate.sh
+  if [[ -z "${CF_TORCH_CUDA_ARCH_LIST:-}" ]]; then
+    echo "CF_TORCH_CUDA_ARCH_LIST is not set. Ensure the correct pytorch is installed and its activation scripts have run."
+    exit 1
   fi
+  export TORCH_CUDA_ARCH_LIST="${CF_TORCH_CUDA_ARCH_LIST}"
+  echo "TORCH_CUDA_ARCH_LIST is set to ${TORCH_CUDA_ARCH_LIST}"
   export FORCE_CUDA=1
 fi
 
